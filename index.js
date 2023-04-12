fe233cbd386fbece9170d3e52e7a4974d3fd8936
Pramitha Dewi <dipramithadewi@gmail.com>
2023-04-12 20:07:57 +0700

    Memperbaiki fetch API

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-const myInput = document.getElementById("inputCountry");
-const myButton = document.getElementById("buttonCountry");
+const myInput = document.getElementById('inputCountry')
+const myButton = document.getElementById('buttonCountry')
+myButton.addEventListener('click', function () {
+  const countries = myInput.value
 
-myButton.addEventListener("click", function() {
-    const country = input.value;
-
-    fetch(`https://covid-193.p.rapidapi.com/statistics?country=${country}`, {
-        "method": "GET",
-        "headers": {
-            'X-RapidAPI-Key': '1fb84f09cemshbc864e1330b4786p122d2fjsnedb135330e4d',
-            'X-RapidAPI-Host': 'covid-193.p.rapidapi.com'
-        }
-    })
+  fetch(`https://covid-193.p.rapidapi.com/statistics?country=${countries}`, {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '1fb84f09cemshbc864e1330b4786p122d2fjsnedb135330e4d',
+      'X-RapidAPI-Host': 'covid-193.p.rapidapi.com'
+    }
+  })
     .then(response => response.json())
     .then(data => {
-        const statistics = data.response[0];
-    
-        document.getElementById("activeCases").textContent = statistics.cases.active;
-        document.getElementById("newCases").textContent = statistics.cases.new;
-        document.getElementById("recoveredCases").textContent = statistics.cases.recovered;
-        document.getElementById("totalCases").textContent = statistics.cases.total;
-        document.getElementById("totalDeaths").textContent = statistics.deaths.total;
-        document.getElementById("totalTests").textContent = statistics.tests.total;
+      const covidData = data.response[0]
+
+      document.getElementById('activeCases').textContent =
+        covidData.cases.active
+      document.getElementById('newCases').textContent = covidData.cases.new
+      document.getElementById('recoveredCases').textContent =
+        covidData.cases.recovered
+      document.getElementById('totalCases').textContent = covidData.cases.total
+      document.getElementById('totalDeaths').textContent =
+        covidData.deaths.total
+      document.getElementById('totalTests').textContent = covidData.tests.total
     })
     .catch(err => {
-        console.error(err);
-        alert("An error occured while retrieving the data");
-    });
-});
+      console.error(err)
+      alert('An error occured while retrieving the data')
+    })
+})
