cd33a1694af3bf9e753d052370b196aabc6f41cc
Pramitha Dewi <dipramithadewi@gmail.com>
2023-04-12 19:43:34 +0700

    Mengedit bagian card

--- index.js
+++ index.js
@@ -1,38 +1,29 @@
-function getCovidData() {
-    const options = {
-        method: 'GET',
-        headers: {
+const myInput = document.getElementById("inputCountry");
+const myButton = document.getElementById("buttonCountry");
+
+myButton.addEventListener("click", function() {
+    const country = input.value;
+
+    fetch(`https://covid-193.p.rapidapi.com/statistics?country=${country}`, {
+        "method": "GET",
+        "headers": {
             'X-RapidAPI-Key': '1fb84f09cemshbc864e1330b4786p122d2fjsnedb135330e4d',
             'X-RapidAPI-Host': 'covid-193.p.rapidapi.com'
         }
-    };
-    const country = document.getElementById("inputCountry").value;
-
-
+    })
+    .then(response => response.json())
+    .then(data => {
+        const statistics = data.response[0];
     
-    if(country) {
-        fetch('https://covid-193.p.rapidapi.com/statistics?country=indonesia', options)
-        .then((response) => response.json())
-        .then((data) => {
-            // Fetch data 
-            const currentData = data[data.length - 1];
-
-            // Access data 
-            const currentActiveCases = currentData.cases.active
-            const currentNewCases = currentData.cases.new
-            const currentRecoveredCases = currentData.cases.recovered
-            const currentTotalCases = currentData.cases.total
-            const currentTotalDeaths = currentData.deaths.total
-            const currentTotalTests = currentData.tests.total
-
-            // Display data 
-            document.getElementById("activeCases").textContent = currentActiveCases.toLocaleString();
-            document.getElementById("newCases").textContent = currentNewCases.toLocaleString();
-            document.getElementById("recoveredCases").textContent = currentRecoveredCases.toLocaleString();
-            document.getElementById("totalCases").textContent = currentTotalCases.toLocaleString();
-            document.getElementById("totalDeaths").textContent = currentTotalDeaths.toLocaleString();
-            document.getElementById("totalTests").textContent = currentTotalTests.toLocaleString();
-        })
-        .catch((error) => console.error(error));
-    }
-}
+        document.getElementById("activeCases").textContent = statistics.cases.active;
+        document.getElementById("newCases").textContent = statistics.cases.new;
+        document.getElementById("recoveredCases").textContent = statistics.cases.recovered;
+        document.getElementById("totalCases").textContent = statistics.cases.total;
+        document.getElementById("totalDeaths").textContent = statistics.deaths.total;
+        document.getElementById("totalTests").textContent = statistics.tests.total;
+    })
+    .catch(err => {
+        console.error(err);
+        alert("An error occured while retrieving the data");
+    });
+});
